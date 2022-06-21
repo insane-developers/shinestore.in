d4d8f1f0646b3c55adf33c1f12f48b9a82f0faed
Elbin Binil <elbinbinil@gmail.com>
2022-06-21 12:59:41 +0000

    Some more code

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="bg-indigo-500 sticky top-0">
+    <div className="bg-indigo-500 sticky top-0 ">
       <nav className="container mx-auto ">
         <div className="sm:flex justify-left">
           <Link href="/">
-            <h2 className="text-3xl px-3 py-3 text-white font-semibold">
+            <h2 className="text-3xl px-3 py-3 cursor-pointer text-white hover:text-slate-300 font-semibold animate-pulse">
               shinestore.in
             </h2>
           </Link>
